5fc93ed9e5ac5173556390434ae174d5e0fd75ae
Lani Hurley <75225771+lanihurley@users.noreply.github.com>
2021-08-22 14:02:49 -0400

    initial commit

---
+++ alh_script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+	/*
+		WA160 Team Project 1
+		Author: Lani Hurley
+		Date: 8/17/2021
+		JavaScript for Classes page
+		A Lightworkers haven
+	
+		Filename: alh_script.js
+	*/
+
+window.alert("'Someone I loved once gave me a box full of darkness. It took me years to understand that this, too, was a gift.'  - Mary Oliver");
+
